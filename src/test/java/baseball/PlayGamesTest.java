e716ec41788507b65b6a4f1a21d2e756f48d8d92
jam-luck <jamluck95@gmail.com>
2023-02-01 23:22:11 +0900

    feat: ParameterizedTest, MethodSource 이용 junit 테스트 추가

--- src/test/java/baseball/PlayGamesTest.java
+++ src/test/java/baseball/PlayGamesTest.java
@@ -1,16 +1,31 @@
 package baseball;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PlayGamesTest {
-    @Test
-    void play(){
-
+    private static Stream<Object[]> inputSet(){
+        return Stream.of(
+                new Object[]{Arrays.asList(1,2,3),1,1,BallStatus.STRIKE},
+                new Object[]{Arrays.asList(1,2,3),1,2,BallStatus.BALL},
+                new Object[]{Arrays.asList(1,2,3),1,4,BallStatus.NOTHING}
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("inputSet")
+    void play(List<Integer> ball, int index, int ballNum, BallStatus expect){
+        Games answers = new Games(ball);
+        BallStatus status = answers.play(new Game(index, ballNum));
+        assertThat(status).isEqualTo(expect);
     }
     @Test
     void strike(){
